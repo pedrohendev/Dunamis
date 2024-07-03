577c107a630809087bb2b97a7ae01301e478c345
Pedro Henrique <164812419+pedrohendev@users.noreply.github.com>
2024-07-03 10:48:03 -0300

    novo

--- javascript/pag002.js
+++ javascript/pag002.js
@@ -1,15 +1,18 @@
 function calcular() {
-  var precoLimpezaComum = 10
-  var precoLimpezaPosObra = 14
-  var tipoLimpeza = String(document.getElementById("tipoLimpeza").value);
-  var metro = Number(document.getElementById('metro').value)
-  if (tipoLimpeza == "comum") {
-    total = metro * precoLimpezaComum;
-  } else if (tipoLimpeza == "posObra") {
-    total = metro * precoLimpezaPosObra;
-  }
-  res.style.currency = "BRL";
-  res.innerHTML = `A área é ${metro} metros quadrados. O custo total para ${
-    tipoLimpeza === "comum" ? "limpeza comum" : "limpeza pós-obra"
-  } é ${total.toFixed(2).replace(".", ",")}.`;
+    var precoLimpezaComum = 10
+    var precoLimpezaPosObra = 14
+    var tipoLimpeza = String(document.getElementById("tipoLimpeza").value);
+    var metro = document.getElementById('metro')
+      if (metro.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+      } else if (tipoLimpeza == "comum") {
+        var metro = Number(metro.value)
+        total = metro * precoLimpezaComum
+      } else if (tipoLimpeza == "posObra") {
+        total = metro * precoLimpezaPosObra
+      }
+
+    res.style.currency = "BRL";
+    res.innerHTML = `A área é ${metro} metros quadrados. O custo total para ${tipoLimpeza === "comum" ? "limpeza comum" : "limpeza pós-obra"} é ${total.toFixed(2).replace(".", ",")}.`;
 }
+
